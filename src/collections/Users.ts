0b09383a9a83c1dff8c2a6fe4baa3a8a89852fab
jonathan hamilton <longjonns@gmail.com>
2024-06-21 19:28:29 +0100

    fixed type import for users

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,13 +1,13 @@
-import type { CollectionConfig } from 'payload'
+import { CollectionConfig } from "payload/types";
 
 export const Users: CollectionConfig = {
-  slug: 'users',
+  slug: "users",
   admin: {
-    useAsTitle: 'email',
+    useAsTitle: "email",
   },
   auth: true,
   fields: [
     // Email added by default
     // Add more fields as needed
   ],
-}
+};
